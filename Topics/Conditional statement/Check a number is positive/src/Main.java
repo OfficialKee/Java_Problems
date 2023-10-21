977f1c5a712fd15b79e65737f7571e6c791b938e
Kee <shikee.maloney@codeimmersives.com>
2023-10-20 21:46:58 -0400

    Day 2

--- Topics/Conditional statement/Check a number is positive/src/Main.java
+++ Topics/Conditional statement/Check a number is positive/src/Main.java
@@ -4,5 +4,12 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
+        int value = scanner.nextInt();
+        if(value > 0){
+            System.out.println("YES");
+        }
+        else if(value <= 0 ){
+            System.out.println("NO");
+        }
     }
 }
---
+++ Topics/Increment and decrement/Decrement all/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    //put your code here
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        int three =scanner.nextInt();
+        int four = scanner.nextInt();
+        one--;
+        two--;
+        three--;
+        four--;
+        System.out.print(one);
+        System.out.print(' ');
+        System.out.print(two);
+        System.out.print(' ');
+        System.out.print(three);
+        System.out.print(' ');
+        System.out.print(four);
+
+
+
+
+    }
+}
