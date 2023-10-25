4b832db7b72e4461aab0135514503b705aedd7ff
Kee <shikee.maloney@codeimmersives.com>
2023-10-24 20:35:09 -0400

    Day 6

---
+++ Topics/Switch statement/Statements/src/Task.java
@@ -0,0 +1,12 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+  }
+  long lo = Integer.MAX_VALUE; // (1)
+  int i = (int) lo; // (2)
+  char ch = (char) i; // (3)
+  double d = ch; // (4)
+  float f = (float) d; // (5)
+}
